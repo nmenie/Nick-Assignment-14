47700237b26ba73a6d7b0247466633e3512d8a0e
kcinm <kcinmenie@gmail.com>
2024-02-16 15:16:20 -0500

    first commit

---
+++ src/main/java/com/codercampus/assignment14/NickAssignment14Application.java
@@ -0,0 +1,13 @@
+package com.codercampus.assignment14;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NickAssignment14Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NickAssignment14Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/codercampus/assignment14/NickAssignment14ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codercampus.assignment14;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NickAssignment14ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
